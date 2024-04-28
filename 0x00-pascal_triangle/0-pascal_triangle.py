f5a66fbd32b8e7b9ba0eebb85d63845d80e4956d
mberrouk <mberrouk@student.1337.ma>
2024-04-28 22:37:50 +0100

    first try

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Generate Pascal's triangle up to the given number of rows.
+"""
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the given number of rows.
+
+    Parameters:
+        n (int): The number of rows to generate in Pascal's triangle.
+
+    Returns:
+        list of lists: - A list of lists of integers representing Pascal's triangle.
+                       - Returns an empty list if n <= 0.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for line in range(1, n + 1):
+        coef = 1
+        row = []
+        for k in range(1, line + 1):
+            row.append(int(coef))
+            coef = coef * (line - k) / k
+        triangle.append(row)
+    return triangle
