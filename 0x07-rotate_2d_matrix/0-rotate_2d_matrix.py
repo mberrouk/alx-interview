561ef520108ee3202551d4d4e989c044c527d66f
mberrouk <mberrouk@student@1337.ma>
2024-11-23 12:48:39 -0800

    handling pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 """
-	Given an n X n 2D matrix, rotate it 90 degrees clockwise.
+Given an n X n 2D matrix, rotate it 90 degrees clockwise.
 """
 
 
 def rotate_2d_matrix(matrix):
-	"""
-		Rotate matrix and edited it.
-	"""
-	matrixLen = len(matrix)
-	newMatrix = []
-	j = 0
+    """ Rotate matrix and edited it.
+    """
+    matrixLen = len(matrix)
+    newMatrix = []
+    j = 0
 
-	while j < matrixLen:
-		i = matrixLen - 1
-		tmprow = []
-		while i >= 0:
-			tmprow.append(matrix[i][j])
-			i -= 1
-		newMatrix.append(tmprow)
-		j += 1
+    while j < matrixLen:
+        i = matrixLen - 1
+        tmprow = []
+        while i >= 0:
+            tmprow.append(matrix[i][j])
+            i -= 1
+        newMatrix.append(tmprow)
+        j += 1
 
-	for i in range(matrixLen):
-		matrix[i] = newMatrix[i]
-
+    for i in range(matrixLen):
+        matrix[i] = newMatrix[i]
